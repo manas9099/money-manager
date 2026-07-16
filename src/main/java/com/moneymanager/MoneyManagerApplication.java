cb51351409fe2f3930781e627a64b7cff63f6d24
Manas Kumar <manaskumar8959@gmail.com>
2026-07-16 22:58:43 +0530

    Update MoneyManagerApplication.java

--- src/main/java/com/moneymanager/MoneyManagerApplication.java
+++ src/main/java/com/moneymanager/MoneyManagerApplication.java
@@ -3,7 +3,7 @@ package com.moneymanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.moneymanager")
 public class MoneyManagerApplication {
 
     public static void main(String[] args) {
